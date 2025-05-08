a2324be7ebbd2b993c18216f1e87197b082fd043
marvellous <120428022+marviecephas@users.noreply.github.com>
2025-05-09 00:32:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let body = document.getElementById('main-container')
+    let icons = document.getElementsByClassName('content-icon');
+    if(!body || !icons){
+        console.error('failed to load DOM elements');
+        return;
+    }
+    
+    const getHeight = (answer) => {
+        let height = 0;
+        let display = answer.style.display;
+        answer.style.display = "block";
+        answerHeight = Number(answer.offsetHeight);
+        answer.style.display = (display === "none") ? "none" : "block";
+        return answerHeight;
+    }
+    const toggleAnswer = (questionIcon) => {
+        questionIcon.parentNode.nextElementSibling.style.display = (questionIcon.src === "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/faq-accordion/icon-plus.svg") ? "block" : "none";
+        
+        questionIcon.src = (questionIcon.src == "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/faq-accordion/icon-plus.svg") ? "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/faq-accordion/icon-minus.svg" : "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/faq-accordion/icon-plus.svg";
+        
+        let answerHeightToAdd = 0, answerHeightToRemove = 0, height = 0, totalHeight = 0;
+        height = getHeight(questionIcon.parentNode.nextElementSibling);
+        
+        answerHeightToAdd = ((questionIcon.src === "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/faq-accordion/icon-minus.svg") && (questionIcon.parentNode.nextElementSibling.style.display === "block")) ? height : 0;
+        
+        answerHeightToRemove = ((questionIcon.src === "file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/faq-accordion/icon-plus.svg") && (questionIcon.parentNode.nextElementSibling.style.display === "none")) ? -1 * height : 0;
+         
+        totalHeight = body.offsetHeight + answerHeightToAdd + answerHeightToRemove;  
+        body.style.minHeight = `${totalHeight}px`;       
+    }
+    for (let icon of icons){
+        icon.addEventListener('click', function(){toggleAnswer(icon)});
+        }
+});
